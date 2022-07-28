5d61e4a5a06748a90840147b63d48518cb872fa2
christopoulos-g <gr_christopoulos@outlook.com>
2022-07-28 15:52:35 +0300

    Final Version( Might redo )

--- javascript.js
+++ javascript.js
@@ -1,36 +1,7 @@
 let slider = document.getElementById("myRange");
 let sliderOutput = document.getElementById("value_h");
 var colorS = 'red'; // Only var works with window.colorS //
-
-let colorSelection = (color) => { // if statements will return gird_item_active color? //
-    if(color == "red"){
-        window.colorS = 'red';
-    }else if(color == "yellow"){
-        window.colorS = 'yellow';
-    }else if(color == "orange"){
-        window.colorS = 'orange';
-    }else if(color == "green"){
-        window.colorS = 'green';
-    }else if(color == "blue"){
-        window.colorS = 'blue';
-    }else if(color == "purple"){
-        window.colorS = 'purple';
-    }else if(color == "white"){
-        window.colorS = 'white';
-    }else if(color == "black"){
-        window.colorS = 'black';
-    }else if(color == "gray"){
-        window.colorS = 'gray';
-    }else if(color == "aqua"){
-        window.colorS = 'aqua';
-    }else if(color == "brown"){
-        window.colorS = 'brown';
-    }else if(color == "pink"){
-        window.colorS = 'pink';
-    }
-    console.log (colorS);
-}
-
+const gridContainer = document.getElementsByClassName("grid_container")[0];
 
 gridCreate(32);     
 sliderOutput.innerHTML = slider.value;
@@ -42,21 +13,36 @@ slider.oninput = function () {
 }
 
 
-const colorApply = (e) => {
-    if (e.target.classList.contains('grid_item')) {
-
-      e.target.classList.toggle(colorS);
-      console.log('inside if clicked');
-    }
-    console.log('outside clicked');
+let colorSelection = (color) => { // if statements will return gird_item_active color? //
+    clearFocus();
+    document.getElementById(color).classList.toggle('colors_focus');
+    window.colorS = color;
 }
 
-document.querySelector('.grid_container').addEventListener('click', colorApply);
+function clearFocus(){
+    document.getElementById("red").classList.remove("colors_focus");
+    document.getElementById("yellow").classList.remove("colors_focus");
+    document.getElementById("orange").classList.remove("colors_focus");
+    document.getElementById("green").classList.remove("colors_focus");
+    document.getElementById("blue").classList.remove("colors_focus");
+    document.getElementById("purple").classList.remove("colors_focus");
+    document.getElementById("white").classList.remove("colors_focus");
+    document.getElementById("black").classList.remove("colors_focus");
+    document.getElementById("gray").classList.remove("colors_focus");
+    document.getElementById("aqua").classList.remove("colors_focus");
+    document.getElementById("brown").classList.remove("colors_focus");
+    document.getElementById("pink").classList.remove("colors_focus");
+    document.getElementById("eraser").classList.remove("colors_focus");
+}
 
+
+function gridClearButton(){
+    gridClear();
+    gridCreate(slider.value);
+}
 
 
 function gridClear() {
-    const gridContainer = document.getElementsByClassName("grid_container")[0];
     while (gridContainer.firstChild) {
         gridContainer.removeChild(gridContainer.lastChild);
     }
@@ -64,15 +50,38 @@ function gridClear() {
 
 
 function gridCreate(size) {
-    const gridContainer = document.getElementsByClassName("grid_container")[0];
     gridContainer.style.setProperty('grid-template-columns', 'repeat(' + size + ', auto )');
     gridContainer.style.setProperty('grid-template-rows', 'repeat(' + size + ', auto');
 
     for(let i = 0; i < size*size; i++) {
         const gridChild = document.createElement("div");
         gridChild.className = "grid_item";
-        const gridChild_t = document.createTextNode(" ");
-        gridChild.appendChild(gridChild_t);
         document.getElementsByClassName("grid_container")[0].appendChild(gridChild);
-    } 
+    }
 }
+
+
+const colorApply = (e) => {
+    if (e.target.classList.contains('grid_item')) {
+        if(colorS == "eraser"){
+            console.log("it works in");
+            e.target.classList.remove('red');
+            e.target.classList.remove('yellow');
+            e.target.classList.remove('orange');
+            e.target.classList.remove('green');
+            e.target.classList.remove('blue');
+            e.target.classList.remove('purple');
+            e.target.classList.remove('white');
+            e.target.classList.remove('black');
+            e.target.classList.remove('gray');
+            e.target.classList.remove('aqua');
+            e.target.classList.remove('brown');
+            e.target.classList.remove('pink');
+        }else{
+        e.target.classList.toggle(colorS);
+        console.log("it works out");
+        }
+    }
+}
+
+document.querySelector('.grid_container').addEventListener('click', colorApply);
